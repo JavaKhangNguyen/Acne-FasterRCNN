21ff0de53a85c35d4b733ef4e167c8538d9cb97d
Pham Duc Dat <phamducdat@Dat-Phams-MacBook-Pro.local>
2024-05-29 11:09:48 +0700

    push slurm

--- run.sh
+++ run.sh
@@ -5,11 +5,12 @@
 #SBATCH --ntasks=1     
 #SBATCH --cpus-per-task=4      
 #SBATCH --mem=64G   
-#SBATCH --partition=a100    
+#SBATCH --partition=gtx1080    
 #SBATCH --gres=gpu:2    
 #SBATCH --time=2-00:00:00       
 #SBATCH --output=fasterCNN%j.out   
 #SBATCH --error=fasterCNN%j.err       
+#SBATCH --exclusive
 
 module load cuda/10.2           
 
