2607b510bc5d8540b57e2b5e62fbbc659a73feda
Pham Duc Dat <phamducdat@Dat-Phams-MacBook-Pro.local>
2024-05-29 10:58:48 +0700

    push slurm

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1     
 #SBATCH --cpus-per-task=40       
 #SBATCH --mem=64G   
-#SBATCH --partition=a100    
+#SBATCH --partition=amd    
 #SBATCH --gres=gpu:2    
 #SBATCH --time=2-00:00:00       
 #SBATCH --output=fasterCNN%j.out   
